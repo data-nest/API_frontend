cadcd0b44c8897d3e567b2d7ce4947a35b94fa51
jeeva2334 <jeeva.v910@gmail.com>
2024-11-04 18:21:53 +0530

    cors fix

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const connectDB = require('./db');
 const authRoutes = require('./authRoutes');
+const cors = require('cors');
 
 const app = express();
 const PORT = 5000;
 
 // Connect to MongoDB
 connectDB();
-
+app.use(cors());
 // Middleware
 app.use(express.json());
 
